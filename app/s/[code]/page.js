8d1fc75cdcc0afde551a7be66847e9fa3168a964
kumargyanendracse <106873389+kumargyanendracse@users.noreply.github.com>
2026-01-10 21:22:40 +0530

    Update page.js

--- app/s/[code]/page.js
+++ app/s/[code]/page.js
@@ -3,14 +3,15 @@ import { useEffect } from "react";
 
 export default function Redirect({ params }) {
   useEffect(() => {
-    const links = JSON.parse(localStorage.getItem("links") || "[]");
-    const match = links.find(l => l.code === params.code);
-
-    if (match) {
-      window.location.href = match.url;
-    } else {
-      alert("Link not found");
-    }
+    fetch(`https://gyani-shortener-api.onrender.com/resolve?code=${params.code}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.url) {
+          window.location.href = data.url;
+        } else {
+          alert("Link not found");
+        }
+      });
   }, []);
 
   return <p style={{ padding: "40px" }}>Redirecting...</p>;
